ccfe76b7c44d5f7dfc28218d4d26416b04ba2988
saifur2233 <saifurnstuiit223344@gmail.com>
2023-01-14 21:25:18 +0600

    data fetch successfully

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import { useState } from "react";
+import React, { useRef, useState } from "react";
+import data from "./data";
 
-function App() {
-  const [count, setCount] = useState(0);
+const App = () => {
+  const [count, setCount] = useState<number>(0);
+  const [text, setText] = useState<string[]>([]);
+
+  const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCount(parseInt(e.currentTarget.value));
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    let amount = count;
+
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
+  };
 
   return (
     <div className="hero min-h-screen bg-base-200">
@@ -10,29 +30,32 @@ function App() {
           <h1 className="text-5xl text-secondary font-bold">
             TIRED OF BORING LOREM IPSUM?
           </h1>
-          <form className="flex justify-center py-6">
+          <form onSubmit={handleSubmit} className="flex justify-center py-6">
             <div className="form-control">
-              <label className="input-group">
-                <input
-                  type="number"
-                  min="1"
-                  max="8"
-                  placeholder="Paragraphs"
-                  className="input input-bordered w-full"
-                />
-                <span>Generate</span>
-              </label>
+              <input
+                type="number"
+                min="0"
+                max="8"
+                placeholder="Paragraphs"
+                className="input input-bordered w-full"
+                name="amount"
+                value={count}
+                onChange={handleAmountChange}
+              />
+              <button type="submit" className="btn btn-secondary">
+                Generate
+              </button>
             </div>
           </form>
           <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+            {text.map((item, index) => {
+              return <p key={index}>{item}</p>;
+            })}
           </p>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
